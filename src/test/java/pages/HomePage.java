44f6dd569bf36fc54f84fca2bd23e70d57c0b378
cihatcni <cihatc52@gmail.com>
2021-01-07 16:23:21 +0300

    Small changes.

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -16,7 +16,6 @@ public class HomePage {
     By searchButtonInBar = By.xpath("//*[@id=\"header-search\"]/div[1]/div/div/form/div/button/i");
     By pasajButton = By.xpath("/html/body/header/div[2]/div/nav/a[1]");
 
-    //Method to click login button
     public void clickSearchButton() {
         driver.findElement(searchButton).click();
     }
@@ -33,5 +32,5 @@ public class HomePage {
     public void clickPasaj() {
         driver.findElement(pasajButton).click();
     }
-    
+
 }
